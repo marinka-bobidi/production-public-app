66654a82cce7a3e56e96c06c7b640c321764d1c8
Alexey Denisov <bauworkml@gmail.com>
2025-02-06 15:00:26 +0300

    fix: исправил сторибук

--- src/components/Motivation/SectionVolunteerBonuses/SectionVolunteerBonuses.stories.tsx
+++ src/components/Motivation/SectionVolunteerBonuses/SectionVolunteerBonuses.stories.tsx
@@ -20,9 +20,36 @@ type Story = StoryObj<typeof meta>;
 
 // More on interaction testing: https://storybook.js.org/docs/writing-tests/interaction-testing
 export const WithStateChanged: Story = {
+	args: {
+		className:'',
+		tasksLink: 'https://ya.ru/',
+		bonusCardProps: [
+			{
+				logo: 'imageDodoPizza',
+				caption: "Додокоины в Додо Пицца",
+				category: "Кафе и рестораны",
+				points: 2,
+				onClickCard: () => alert("Клик по карточке Default"),
+			},
+			{
+				logo: 'imageAzbuka',
+				caption: "Промокод на книги",
+				category: "Книги",
+				points: 5,
+				onClickCard: () => alert("Клик по карточке Default"),
+			},
+			{
+				logo: 'imageBbe',
+				caption: "Доступ к дизайн-библиотеке на месяц",
+				category: "Образование",
+				points: 8,
+				onClickCard: () => alert("Клик по карточке Default"),
+			},
+		]
+	},
 	play: async ({ canvasElement }) => {
 		const canvas = within(canvasElement);
-		const element = canvas.getByTestId('SectionVolunteerMotivation');
+		const element = canvas.getByTestId('SectionVolunteerBonuses');
 		await expect(element).toBeInTheDocument();
 	},
 };
